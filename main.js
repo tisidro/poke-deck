56d4996b908cadbd1efc03b754afbef09b1a58c2
Terri Isidro <terri.isidro@gmail.com>
2022-09-11 01:10:00 -0400

    feat: create fetch functions for api
    create functions that: fetch pokemon by id from api object, return a
    specified number of pokemon from array, and generate pokemon cards by
    creating the card, the image wrapper, the image and adds name/pokemon
    number to card.

--- main.js
+++ main.js
@@ -2,20 +2,47 @@
 const mainWrapper = document.querySelector('.main-wrapper')
 
 //function retrieves pokemon data from api
-function getPokemon (id) {
+function fetchPokemon (id) {
   fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+      generatePokemon(data)
+    })
 }
 
 // console.log(getPokemon(1))
 
-function getMultiplePokemon (items) {
-  for (let i = 1; i <= items; i++) {
-    getPokemon(i)
+function fetchPokemons (number) {
+  for (let i = 1; i <= number; i++) {
+    fetchPokemon(i)
   }
 }
 
 // getMultiplePokemon(6)
 
-function generatePokemon (pokemon) {}
+function generatePokemon (pokemon) {
+  const card = document.createElement('div')
+  card.classList.add('pokemon-card')
+
+  const spriteWrapper = document.createElement('div')
+  spriteWrapper.classList.add('sprite-wrapper')
+
+  const sprite = document.createElement('img')
+  sprite.src = pokemon.sprites.front_default
+
+  spriteWrapper.appendChild(sprite)
+
+  const number = document.createElement('p')
+  number.textContent = `#${pokemon.id.toString().padStart(3, 0)}`
+
+  const name = document.createElement('p')
+  name.classList.add('name')
+  name.textContent = pokemon.name
+
+  card.appendChild(spriteWrapper)
+  card.appendChild(number)
+  card.appendChild(name)
+
+  mainWrapper.appendChild(card)
+}
+fetchPokemons(12)
